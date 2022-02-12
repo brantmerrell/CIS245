258fd2bff340255176c6d183f21621835add7ea4
brantmerrell <brant.merrell@gmail.com>
2022-02-12 21:47:51 +0000

    project linting & unit-test outline

--- project.py
+++ project.py
@@ -1,14 +1,21 @@
-'''project to get and print weather data.'''
+'''
+project to get and print weather data from
+https://openweathermap.org/current#data
 
-import re
-import requests
+Feedback from pseudocode draft:
+ - lack of input validation to make sure a zipcode is a zipcode or a city name is alphabetical.
+ - pass units=imperial|other into query string and let webservice do conversion work.
+'''
+
 import unittest
 from unittest import mock
+#import requests
 import pylint
 # To-Do: app ID
     # hard-code?
     # get automatically?
     # get interactively?
+
 
 def prompt_location_info():
     '''
@@ -25,36 +32,38 @@ def prompt_location_info():
     result['zip'] = input('zip: ')
     return result
 
+
 def location_info_is_valid(location):
     '''
     evaluate location info; call prompt_location_info if location info is invalid
     '''
-    errors = ''
-    if type(location) is not dict:
-        return( (False, 'location object must be a dict') )
-    for attribute in ['city','state','zip']:
+    result = (True, 'no errors detected')
+    if not isinstance(location, dict):
+        return (False, 'location object must be a dict')
+    for attribute in ['city', 'state', 'zip']:
         if attribute not in location.keys():
-            return( (False, 'location dict must contain city, state, and zip keys') )
-        if type(location[attribute]) is not str:
-            return( (False, f'location object {attribute} must be a string') )
+            result = (False, 'location dict must contain city, state, and zip keys')
+        if not isinstance(location[attribute], str):
+            result = (False, f'location object {attribute} must be a string')
     if not location['zip'].isnumeric():
-        return ( (False, 'zip must be numeric') )
+        result = (False, 'zip must be numeric')
     if location['zip'].count('') != 6:
-        return ( (False, 'zip must be a 5-digit number') )
+        result = (False, 'zip must be a 5-digit number')
     if not location['city'].isalpha():
-        return ( (False, 'city must be alphabetical') )
+        result = (False, 'city must be alphabetical')
     if not location['state'].isalpha():
-        return ( (False, 'state must be alphabetical') )
-    return (True, 'no errors detected')
+        result = (False, 'state must be alphabetical')
+    return result
 
 
 
 
 # evaluate whether info is valid and sufficient to construct query parameters
+def construct_url():
+    return None
     # if so, proceed to step 2
     # if not, return to step 1 with message to user
-
-# step 3 get_forecast(): send the query parameters to 
+# step 3 get_forecast(): send the query parameters to
     # units = imperial|metric|other
     # api.openweathermap.org/data/2.5/weather?q={city,state}|{zip}&{app_id}
     # inform user if connection was successful
@@ -86,12 +95,21 @@ def location_info_is_valid(location):
     # distinguish whether app is being called from the python repl or the command line.
 
 
-class testProject(unittest.TestCase):
-    def test_hello_world(self):
-        '''Ascertain that nothing breaks on run'''
-        self.assertTrue(True)
+class TestProject(unittest.TestCase):
+    '''
+    class for all unit tests of this project:
+        - test_prompt_location_info
+        - test_location_info_is_valid
+        - test_construct_url
+    '''
+
 
     def test_prompt_location_info(self):
+        '''
+        function "prompt_location_info" should
+            - prompt for city, state, zip
+            - return dictionary of string values for city, state, zip
+        '''
         mock_args = ['Austin', 'TX', '78727']
         with mock.patch('builtins.input') as mocked_input:
             mocked_input.side_effect = mock_args
@@ -100,39 +118,97 @@ class testProject(unittest.TestCase):
         self.assertEqual(result['state'], 'TX')
         self.assertEqual(result['zip'], '78727')
 
-    def test_location_info_is_valid__zip(self):
-        good_zip_location = {
+
+    def test_location_info_is_valid(self):
+        '''
+            function "location_info_is_valid" should
+                - return (bool, message) tuple
+                - require numeric zip
+                - require 5-character zip
+                - require alphabetic city
+                - require alphabetic state
+        '''
+        good_location = {
             'city': 'Austin',
             'state': 'TX',
             'zip': '78727'
             }
-        bad_zip_location = {
+        location_as_list = ['Austin', 'TX', '78727']
+        location_with_numeric_city = {
+            'city': 'Aus1in',
+            'state': 'TX',
+            'zip': '78727'
+            }
+        location_with_numeric_state = {
+            'city': 'Austin',
+            'state': '5X',
+            'zip': '78727'
+            }
+        location_with_letter_zip = {
             'city': 'Austin',
             'state': 'TX',
             'zip': '787d7'
             }
-        print(location_info_is_valid(good_zip_location))
-        self.assertTrue(location_info_is_valid(good_zip_location)[0])
-        self.assertFalse(location_info_is_valid(bad_zip_location)[0])
+        location_with_short_zip = {
+            'city': 'Austin',
+            'state': 'TX',
+            'zip': '7877'
+            }
+        self.assertTrue(location_info_is_valid(good_location)[0])
+        self.assertEqual(
+            location_info_is_valid(location_as_list),
+            (False, 'location object must be a dict'))
+        self.assertEqual(
+            location_info_is_valid(location_with_numeric_city),
+            (False, 'city must be alphabetical'))
+        self.assertEqual(
+            location_info_is_valid(location_with_numeric_state),
+            (False, 'state must be alphabetical'))
+        self.assertEqual(
+            location_info_is_valid(location_with_letter_zip),
+            (False, 'zip must be numeric'))
+        self.assertEqual(
+            location_info_is_valid(location_with_short_zip),
+            (False, 'zip must be a 5-digit number'))
+
+
+    def test_construct_url(self):
+        '''
+        function 'construct_url' should
+            - until location is valid:
+                - call prompt_location_info
+                - validate using 'location_info_is_valid'
+                - validate user intention
+            - construct query string
+            - return URL
+        '''
+        return None
+
+
+    def test_get_weather(self):
+        '''
+        function 'get_weather' should
+            - API token?
+            - call url
+            - return request as dict
+        '''
+        return None
+
+    def test_display_weather(self):
+        '''
+        function 'display weather' should
+            - select attributes
+            - convert dates to human-readable?
+            - other human-readable?
+            - print and return table
+        '''
+        return None
+
 
 print(pylint.epylint.py_run('project.py'))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
     # command-line args?
 else:
     unittest.main(module='primary_colors_strings', exit=False)
-
-
-'''
-Feedback from pseudocode draft:
-Great work on your flow chart. I've attached it here just for record keeping purposes.
-
-My only comments on your flow chart concern the lack of input validation to make sure a zipcode is a zipcode or a city name is alphabetical -- or whatever you determine to be valid string input for the city name.
-
-Also, less of a problem, more of a tip, you seem to have invested a bunch of effort in converting units and formatting data in your flow chart.
-
-If you review the API documentation you will see that you can pass units=imperial into your query string and the web service will provide the data in imperial (or whatever other unit of measure the API accepts).
-
-https://openweathermap.org/current#data
-'''
